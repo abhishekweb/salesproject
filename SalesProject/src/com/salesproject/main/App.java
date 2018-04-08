ec14e6a3fb299a057368370828e4a3617728c68c
Abhishek <abhishek.kumar16@cognizant.com>
2018-04-08 15:57:45 +0530

    Second checked in

--- SalesProject/src/com/salesproject/main/App.java
+++ SalesProject/src/com/salesproject/main/App.java
@@ -1,11 +1,16 @@
 package com.salesproject.main;
 
+import com.salesproject.main.dao.Sales;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("This is main class");
+		Sales obj = new Sales();
+		obj.setCategory("Online");
+		obj.setDescription("For online");
 
 	}
 
